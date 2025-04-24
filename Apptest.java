44fb442c1703b50bb9901a2663e6fe5cd089a1b5
sumanth <sumanths9902@gmail.com>
2025-04-24 12:18:42 +0530

    Add files via upload
    committing my project

---
+++ Apptest.java
@@ -0,0 +1,21 @@
+package sumanth;
+import org.testng.Assert;
+
+import org.testng.annotations. Test;
+public class Apptest{	
+	@Test
+    public void testloginl()
+    {
+    	App myapp=new App();
+    	Assert.assertEquals (0, myapp.userLogin("abc", "abc123"));
+    }
+    
+	@Test
+    public void testlogin2()
+    {
+    	App myapp=new App();
+
+    	Assert.assertEquals (1,myapp.userLogin("abc", "abc@123"));
+    }
+}
+
